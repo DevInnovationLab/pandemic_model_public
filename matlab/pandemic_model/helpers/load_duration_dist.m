8926e02af5b1f11a7af469322a14046a6b791605
sq46 <squaade@uchicago.edu>
2025-03-14 18:27:13 -0400

    add max duration to matlab duration dist config

--- matlab/pandemic_model/helpers/load_duration_dist.m
+++ matlab/pandemic_model/helpers/load_duration_dist.m
@@ -4,7 +4,7 @@ function duration_dist = load_duration_dist(config_path)
     end
 
     % Load parametrized duration distribution from YAML
-    config = yaml.loadFile(config_path); % Can later add min/max durations
-    duration_dist = ParametrizedDurationDist(config.dist_family, config.params);
+    config = yaml.loadFile(config_path);
+    duration_dist = ParametrizedDurationDist(config.dist_family, config.params, config.max_duration);
 
 end
