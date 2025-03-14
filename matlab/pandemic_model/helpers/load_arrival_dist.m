86220b002d1915c07b7843298ef5e3e9219d66dd
sq46 <squaade@uchicago.edu>
2025-03-14 18:27:30 -0400

    add janky false positive adjustment

--- matlab/pandemic_model/helpers/load_arrival_dist.m
+++ matlab/pandemic_model/helpers/load_arrival_dist.m
@@ -1,14 +1,16 @@
-function [metric, arrival_dist] = load_arrival_dist(arrival_dist_config_path)
+function [metric, arrival_dist] = load_arrival_dist(arrival_dist_config_path, false_positive_rate)
     % Load arrival distribution from YAML config file into MEVD object
     %
     % Parameters:
     %   arrival_dist_config_path - Path to YAML config file containing arrival distribution parameters
+    %   false_positive_rate - False positive rate
     %
     % Returns:
     %   arrival_dist - MEVD object representing the arrival distribution
     
     arguments
         arrival_dist_config_path (1,1) string
+        false_positive_rate (1,1) double
     end
 
     % Load arrival distribution config from YAML
@@ -21,6 +23,12 @@ function [metric, arrival_dist] = load_arrival_dist(arrival_dist_config_path)
     base_dist_params = arrival_dist_config.base_dist_params;
     truncation = arrival_dist_config.truncation;
     upper_bound = arrival_dist_config.upper_bound;
+
+    % False positive adjustment, this is hacky
+    false_pos_add = ceil(sum(window_counts) * false_positive_rate) / (1 - false_positive_rate);
+    false_pos_increment = [ones(false_pos_add, 1); zeros(height(window_counts) - false_pos_add, 1)];
+    assert(isequal(size(false_pos_increment), size(window_counts)));
+    window_counts = window_counts + false_pos_increment;
     
     % Create MEVD object
     arrival_dist = MEVD(window_counts, ...
